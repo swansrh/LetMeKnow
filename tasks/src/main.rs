698688d994cfcc5a8f47a75f9d28da047ac52141
swansrh <50853358+swansrh@users.noreply.github.com>
2024-07-16 18:18:00 +1000

    Update main.rs

--- tasks/src/main.rs
+++ tasks/src/main.rs
@@ -1,9 +1,63 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
     logo_print();
-    main_menu()
+    show_tasks();
+    main_menu();
+}
+
+fn main_menu() {
+
+    loop{//begining of loop for menu
+    println!("\nWhat would you like to do? (Type 'Help' for help)");
+    let user_input = get_input(); //gets user input
+    //println!("{}", user_input); //just for debug to see what was input by the user
+
+    match user_input.as_str(){
+        "Help" => help_menu(),
+        "help" => help_menu(),
+        "Edit" => println!("Edit a task"),
+        "edit" => println!("Edit a task"),
+        "Detail" => println!("Detail view"),
+        "detail" => println!("Detail view"),
+        "Exit" => break,
+        "exit" => break,
+        "q" => break,
+        _ => println!("Please input a valid option. Type 'Help' for a list of available commands."),
+    }
+    }// while loop end
+}
+
+fn get_input() -> String{
+    let mut s = String::new(); 
+    io::stdin().read_line(&mut s).expect("Failed to read line");
+    
+    if s.ends_with("\n") { //these statements remove the newline characters at the end of the string
+        s.pop();
+        //println!("Removed1"); //used for debug
+        if s.ends_with("\r") {
+            s.pop();
+            //println!("Removed2"); //used for debug
+        }
+    }
+    return s;
+}
+
+fn show_tasks() {//used to show the tasks
+
+    println!("\nSee current tasks below: \n");
+    for i in 1..10{
+        println!("SICK TASK NUMBER: {}", i);
+    }
+}
+
+fn help_menu() {
+    logo_print();
+    println!("\nLetMeKnow Version 0.0.1 Windows Build\nThese commands have been defined internally. Type 'Help' at anytime to see this list.");
+    println!("\nAdd\n     Allows the user to add an aditional task to the list");
+    println!("\Edit\n     Allows a user to edit an existing task");
+    println!("\nExit\n     Quits the program");
+    println!("\nDetails\n     Navigates to a specific task to get additional information");
 }
 
 fn logo_print() { //Prints the logo
@@ -14,25 +68,4 @@ fn logo_print() { //Prints the logo
     println!(" \\ V  V /| | | | (_) | | | || (_| |  __/\\ V / ");
     println!("  \\_/\\_/ |_|_|  \\___/|_| |_(_)__,_|\\___| \\_/  ");
     println!("\nWelcome to Let Me know, the task management CLI tool")
-}
-
-fn main_menu() {
-    let mut user_input = String::new(); //used for input
-    println!("\nPlease see pending tasks below:\n");
-    show_tasks();
-    println!("\nWhat would you like to do? (Type 'Help' for help)");
-
-    io::stdin().read_line(&mut user_input).expect("Failed to read line");
-
-    println!("{}", user_input)
-}
-
-fn show_tasks() {//used to show the tasks
-    for i in 1..10{
-        println!("SICK TASK NUMBER: {}", i);
-    }
-}
-
-fn help_menu() {
-    println!("Commands")
 }
